19b459cfdd166dd5e2de4857ba11e1bed2ef1695
Hideraldo Rodrigues Marciano <95453443+Hide71@users.noreply.github.com>
2025-12-09 13:01:17 -0300

    inserindo arquivo principal

---
+++ principal.js
@@ -0,0 +1,9 @@
+var bunners =["Os melhores do Brasil!", "Qualidade e preço baixo!"];
+var bunnerAtual = 0;
+
+function trocarBunner(){
+	bunnerAtual = (bunnerAtual + 1) % 2;
+	document.querySelector("h2#mensagem").textContent = 
+	bunners[bunnerAtual];
+}
+setInterval(trocarBunner, 2000);
